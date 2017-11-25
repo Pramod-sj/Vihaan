78610fe1b4d788aa9f6d0e22334318d7ed3e4f56
pramod-sj <pramdosinghjantwal@gmail.com>
2017-11-25 19:54:40 +0530

    added about page

---
+++ app/src/main/java/registrationform/com/registrationform/aboutapp_Activity.java
@@ -0,0 +1,16 @@
+package registrationform.com.registrationform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pramod_sj on 25/11/17.
+ */
+
+public class aboutapp_Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.aboutapp);
+    }
+}
